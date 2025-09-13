ebf48f327ccbdb4e035e3cc90f8aa8c48f23fea6
Striender <striender1809@gmail.com>
2025-09-14 00:37:54 +0530

    Added results, statistics, and build outputs

---
+++ ChampSim/Other_PF/obj/prefetcher/l1d_prefetcher.d
@@ -0,0 +1,14 @@
+obj/prefetcher/l1d_prefetcher.o: prefetcher/l1d_prefetcher.cc \
+ inc/ooo_cpu.h inc/cache.h inc/memory_class.h inc/champsim.h inc/block.h \
+ inc/instruction.h inc/set.h inc/page_table_walker.h inc/cache.h \
+ prefetcher/ipcp_table_sizes.h
+inc/ooo_cpu.h:
+inc/cache.h:
+inc/memory_class.h:
+inc/champsim.h:
+inc/block.h:
+inc/instruction.h:
+inc/set.h:
+inc/page_table_walker.h:
+inc/cache.h:
+prefetcher/ipcp_table_sizes.h:
